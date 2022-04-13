317f91b47f5c8144da85e23b939d19aab498f1d1
joecoxstrong <joecoxstrong@gmail.com>
2022-04-13 16:56:46 -0400

    Created serializer with classes for Comment and Reply

---
+++ backend/drf_jwt_backend/comments/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Comment
+from .models import Reply
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'video_id', 'text', 'likes', 'dislikes']
+        
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Reply
+        fields = ['user','comment','text']
