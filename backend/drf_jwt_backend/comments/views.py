e1ef8f283b859f8891a3ab256d0b435c8b404bde
Chris Creamer <chrisc1983@gmail.com>
2022-04-14 09:54:41 -0700

    got the views function working again after breaking it

--- backend/drf_jwt_backend/comments/views.py
+++ backend/drf_jwt_backend/comments/views.py
@@ -11,9 +11,9 @@ from .serializer import CommentSerializer, ReplySerializer
 @permission_classes([AllowAny])
 def get_all_comments (request):
     if request.method == 'GET':
-        #video_params = request.query_params.get('video_id')
+      #video_params = request.query_params.get('oYzHlvI7bI8')
         comments = Comment.objects.all()
-        #comments = comments.filter(video_id=video_params)
+    #comments = comments.filter(video_id=video_params)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
 
