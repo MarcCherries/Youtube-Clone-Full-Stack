87e991f5a6e8e91f1f5d8f8b642bd27f55a20e29
joecoxstrong <joecoxstrong@gmail.com>
2022-04-18 12:33:43 -0400

    Added search bar and functionality

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -8,6 +8,7 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
+  const [searchItem, setSearchItem] = useState()
   const [videoData, setVideoData] = useState({
     "kind": "youtube#searchListResponse",
     "etag": "eaCOzyVN0arwnWgwpKqs6OY4O9Q",
@@ -78,23 +79,43 @@ const HomePage = () => {
   // }, [token]);
 
   
-  useEffect(() => {
-    const fetchVideoData = async () => {
-      try {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?type=video&key=AIzaSyC9RLs3URCNZr2H68witQ7MUJCN2c89pmw
-        `);
-        setVideoData(response.data);
-        console.log(response.data)
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    fetchVideoData();
-  }, [token]);
+  // useEffect(() => {
+  //   const fetchVideoData = async () => {
+  //     try {
+  //       let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=babyshark&type=video&key=AIzaSyC9RLs3URCNZr2H68witQ7MUJCN2c89pmw&part=snippet
+  //       `);
+  //       setVideoData(response.data);
+  //       console.log(response.data)
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   };
+  //   fetchVideoData();
+  // }, [token,searchItem]);
+  
+  async function handleSubmit(event) {
+    event.preventDefault()
+    try {
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchItem}&type=video&key=AIzaSyC9RLs3URCNZr2H68witQ7MUJCN2c89pmw&part=snippet
+      `);
+      setVideoData(response.data);
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      
+      <form className="searchForm" onSubmit={handleSubmit}>
+        <input type="text" name="search" value={searchItem} onChange={(event)=>setSearchItem(event.target.value) }>
+        
+        </input>
+        <button>
+SEARCH
+        </button>
+      </form>
       <iframe width='500' height='350' src={`https://www.youtube.com/embed/${videoData.items[0].id.videoId}`}></iframe>
       <iframe width='500' height='350' src={`https://www.youtube.com/embed/${videoData.items[1].id.videoId}`}></iframe>
       <iframe width='500' height='350' src={`https://www.youtube.com/embed/${videoData.items[2].id.videoId}`}></iframe>
