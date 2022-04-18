112ffdf4c082d2f068818ecc8985233a8a671a4c
joecoxstrong <joecoxstrong@gmail.com>
2022-04-18 12:01:40 -0400

    Updated HomePage.js by adding useEffect to pull videos from youtube through a google API call + added iframe to verify videos would display and play

--- backend/drf_jwt_backend/comments/views.py
+++ backend/drf_jwt_backend/comments/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Comment, Reply
-from .serializer import CommentSerializer, ReplySerializer, VideoCommentSerializer, NewCommentSerializer
+from .serializer import CommentSerializer, ReplySerializer
 from rest_framework import status
 
 
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -8,32 +8,97 @@ const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [videoData, setVideoData] = useState({
+    "kind": "youtube#searchListResponse",
+    "etag": "eaCOzyVN0arwnWgwpKqs6OY4O9Q",
+    "nextPageToken": "CAUQAA",
+    "regionCode": "US",
+    "pageInfo": {
+        "totalResults": 1000000,
+        "resultsPerPage": 5
+    },
+    "items": [
+        {
+            "kind": "youtube#searchResult",
+            "etag": "krZhLtdi_x6JALjr9y0F6Fsbp-E",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "YQHsXMglC9A"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "RDbKrp81fOmD8r9it_jdwX-HuVo",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "mHONNcZbwDY"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "24_6pWZQYFbEO5vc42J98MpFbNw",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "be12BC5pQLE"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "-vkKMXvyCi-E_bCCHp7XDDRCiqY",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "fazMSCZg-mw"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "xopNDBszt3HNeW5-6fIpleYJaXk",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "8wWxejCosj4"
+            }
+        }
+    ]
+});
 
+  // useEffect(() => {
+  //   const fetchCars = async () => {
+  //     try {
+  //       let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+  //         headers: {
+  //           Authorization: "Bearer " + token,
+  //         },
+  //       });
+  //       setCars(response.data);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   };
+  //   fetchCars();
+  // }, [token]);
+
+  
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchVideoData = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        });
-        setCars(response.data);
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?type=video&key=AIzaSyC9RLs3URCNZr2H68witQ7MUJCN2c89pmw
+        `);
+        setVideoData(response.data);
+        console.log(response.data)
       } catch (error) {
         console.log(error.message);
       }
     };
-    fetchCars();
+    fetchVideoData();
   }, [token]);
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
+      
+      <iframe width='500' height='350' src={`https://www.youtube.com/embed/${videoData.items[0].id.videoId}`}></iframe>
+      <iframe width='500' height='350' src={`https://www.youtube.com/embed/${videoData.items[1].id.videoId}`}></iframe>
+      <iframe width='500' height='350' src={`https://www.youtube.com/embed/${videoData.items[2].id.videoId}`}></iframe>
+      <iframe width='500' height='350' src={`https://www.youtube.com/embed/${videoData.items[3].id.videoId}`}></iframe>
     </div>
   );
 };
